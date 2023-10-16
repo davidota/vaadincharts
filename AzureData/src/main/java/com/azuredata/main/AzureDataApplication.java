fb4f830f904610f1e9fa47211ba776a2b5a91f7d
david <david@david-VirtualBox>
2023-10-16 17:58:53 +0200

    Primer commit

---
+++ AzureData/src/main/java/com/azuredata/main/AzureDataApplication.java
@@ -0,0 +1,13 @@
+package com.azuredata.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDataApplication.class, args);
+	}
+
+}
