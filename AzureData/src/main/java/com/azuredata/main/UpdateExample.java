280abc29c47e31eb7af101e5f0ad2cdf1ffb6536
davidota <davidoscar@gmail.com>
2023-10-24 13:36:57 +0200

    Se agrega un nuevo tipo, histo chart para visualizar la prueba de como funciona

--- AzureData/src/main/java/com/azuredata/main/UpdateExample.java
+++ AzureData/src/main/java/com/azuredata/main/UpdateExample.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import com.github.appreciated.apexcharts.ApexCharts;
 import com.github.appreciated.apexcharts.ApexChartsBuilder;
+import com.github.appreciated.apexcharts.config.TitleSubtitle;
 import com.github.appreciated.apexcharts.config.builder.ChartBuilder;
 import com.github.appreciated.apexcharts.config.builder.DataLabelsBuilder;
 import com.github.appreciated.apexcharts.config.builder.PlotOptionsBuilder;
@@ -18,29 +19,73 @@ import com.vaadin.flow.router.Route;
 
 @Route("update")
 public class UpdateExample extends VerticalLayout {
-	Random rd=new Random();
-    public UpdateExample() {
-        ApexCharts chart = ApexChartsBuilder.get().withChart(ChartBuilder.get()
-                .withType(Type.AREA)
-                .build())
-                .withPlotOptions(PlotOptionsBuilder.get()
-                        .withBar(BarBuilder.get()
-                                .withHorizontal(false)
+        Random rd = new Random();
+
+        public UpdateExample() {
+                ApexCharts Linechart = getLineBehavior();
+                ApexCharts Histochart = getHistogramBehavior();
+                Button update = new Button("Update", buttonClickEvent -> {
+                        Linechart.updateSeries(new Series<>(rd.nextDouble(0, 2000), rd.nextDouble(0, 2000),
+                                        rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000),
+                                        rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000),
+                                        rd.nextDouble(0, 2000), rd.nextDouble(0, 2000)));
+                        Notification.show("The chart was updated!");
+                        Histochart.updateSeries(new Series<>(rd.nextDouble(0, 2000), rd.nextDouble(0, 2000),
+                                        rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000),
+                                        rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000),
+                                        rd.nextDouble(0, 2000), rd.nextDouble(0, 2000)));
+                        Notification.show("The chart was updated!");
+                });
+                add(Linechart, Histochart, update);
+        }
+
+        ApexCharts getLineBehavior() {
+                ApexCharts chart = ApexChartsBuilder.get().withChart(ChartBuilder.get()
+                                .withType(Type.AREA)
                                 .build())
-                        .build())
-                .withDataLabels(DataLabelsBuilder.get()
-                        .withEnabled(false)
-                        .build())
-                .withSeries(new Series<>(400.0, 430.0, 448.0, 470.0, 540.0, 580.0, 690.0, 1100.0, 1200.0, 1380.0))
-                .withXaxis(XAxisBuilder.get()
-                        .withCategories()
-                        .build())
-                .build();
-        chart.setHeight("400px");
-        Button update = new Button("Update", buttonClickEvent -> {
-            chart.updateSeries(new Series<>( rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000), rd.nextDouble(0, 2000)));
-            Notification.show("The chart was updated!");
-        });
-        add(chart, update);
-    }
+                                .withPlotOptions(PlotOptionsBuilder.get()
+                                                .withBar(BarBuilder.get()
+                                                                .withHorizontal(false)
+                                                                .build())
+                                                .build())
+                                .withDataLabels(DataLabelsBuilder.get()
+                                                .withEnabled(false)
+                                                .build())
+                                .withSeries(new Series<>(400.0, 430.0, 448.0, 470.0, 540.0, 580.0, 690.0, 1100.0,
+                                                1200.0, 1380.0))
+                                .withXaxis(XAxisBuilder.get()
+                                                .withCategories()
+                                                .build())
+                                .build();
+                chart.setHeight("300px");
+                TitleSubtitle ts = new TitleSubtitle();
+                ts.setText("Line Chart");
+                chart.setTitle(ts);
+                return chart;
+        }
+
+        ApexCharts getHistogramBehavior() {
+                ApexCharts chart = ApexChartsBuilder.get().withChart(ChartBuilder.get()
+                                .withType(Type.HISTOGRAM)
+                                .build())
+                                .withPlotOptions(PlotOptionsBuilder.get()
+                                                .withBar(BarBuilder.get()
+                                                                .withHorizontal(false)
+                                                                .build())
+                                                .build())
+                                .withDataLabels(DataLabelsBuilder.get()
+                                                .withEnabled(false)
+                                                .build())
+                                .withSeries(new Series<>(400.0, 430.0, 448.0, 470.0, 540.0, 580.0, 690.0, 1100.0,
+                                                1200.0, 1380.0))
+                                .withXaxis(XAxisBuilder.get()
+                                                .withCategories()
+                                                .build())
+                                .build();
+                chart.setHeight("300px");
+                TitleSubtitle ts = new TitleSubtitle();
+                ts.setText("Histo Chart");
+                chart.setTitle(ts);
+                return chart;
+        }
 }
